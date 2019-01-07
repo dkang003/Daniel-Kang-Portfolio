3362e47edb68247be45bd18b8528caed4cde62f5
Daniel Kang <d.kang106@gmail.com>
2019-01-06 21:32:12 -0800

    figuring out the navbar

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 console.log("Hi")
 
-$('.burger-lines').click(function() {
-    // $('.burger-menu').css("display", "flex")
-    // ================================================
-    $('.burger-lines').toggleClass('active');
-    // $('.burger-menu').toggle("show");
-    // $('.burger-menu').css("display", "flex")
-    // ================================================
+// $('.burger-lines').click(function() {
+//     // $('.burger-menu').css("display", "flex")
+//     // ================================================
+//     $('.burger-lines').toggleClass('active');
+//     // $('.burger-menu').toggle("show");
+//     // $('.burger-menu').css("display", "flex")
+//     // ================================================
+//     var menu = $('.burger-menu')[0];
+
+//     if (menu.getAttribute('style') === null) {
+//         menu.setAttribute('style', 'display: flex');
+//     } else if (menu.getAttribute('style') === ('display: none')) {
+//         menu.setAttribute('style', 'display: flex');
+//     } else {
+//         menu.setAttribute('style', 'display: none');
+//     }
+// });
+
+$('.fa-bars').click(function() {
     var menu = $('.burger-menu')[0];
 
     if (menu.getAttribute('style') === null) {
@@ -16,4 +28,4 @@ $('.burger-lines').click(function() {
     } else {
         menu.setAttribute('style', 'display: none');
     }
-});
+})
