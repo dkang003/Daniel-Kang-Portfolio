2b0d6e90805f6763de63636ad63ca535e2589b93
Daniel Kang <d.kang106@gmail.com>
2019-01-06 20:36:34 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hi")
+
+$('.burger-lines').click(function() {
+    // $('.burger-menu').css("display", "flex")
+    // ================================================
+    $('.burger-lines').toggleClass('active');
+    // $('.burger-menu').toggle("show");
+    // $('.burger-menu').css("display", "flex")
+    // ================================================
+    var menu = $('.burger-menu')[0];
+
+    if (menu.getAttribute('style') === null) {
+        menu.setAttribute('style', 'display: flex');
+    } else if (menu.getAttribute('style') === ('display: none')) {
+        menu.setAttribute('style', 'display: flex');
+    } else {
+        menu.setAttribute('style', 'display: none');
+    }
+});
