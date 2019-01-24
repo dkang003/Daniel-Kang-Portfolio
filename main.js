e245f5b2d17a7a3d40a7005946d18f11f43e8866
Daniel Kang <d.kang106@gmail.com>
2019-01-23 20:33:15 -0800

    redid my portfolio project with bulma

--- main.js
+++ main.js
@@ -1,31 +1,88 @@
 console.log("Hi")
 
-// $('.burger-lines').click(function() {
-//     // $('.burger-menu').css("display", "flex")
-//     // ================================================
-//     $('.burger-lines').toggleClass('active');
-//     // $('.burger-menu').toggle("show");
-//     // $('.burger-menu').css("display", "flex")
-//     // ================================================
-//     var menu = $('.burger-menu')[0];
+// =============================================================
+// CAROUSEL CODE
+// =============================================================
+// Auto Slideshow function
+// array of project slides
+var projects = $('.project');
+// array of slider dots
+var dots = $('.dot');
+var slideIdx = 0;
 
-//     if (menu.getAttribute('style') === null) {
-//         menu.setAttribute('style', 'display: flex');
-//     } else if (menu.getAttribute('style') === ('display: none')) {
-//         menu.setAttribute('style', 'display: flex');
-//     } else {
-//         menu.setAttribute('style', 'display: none');
-//     }
-// });
-
-$('.fa-bars').click(function() {
-    var menu = $('.burger-menu')[0];
-
-    if (menu.getAttribute('style') === null) {
-        menu.setAttribute('style', 'display: flex');
-    } else if (menu.getAttribute('style') === ('display: none')) {
-        menu.setAttribute('style', 'display: flex');
-    } else {
-        menu.setAttribute('style', 'display: none');
+function showSlides() {
+    for (var i = 0; i < projects.length; i++) {
+        projects[i].style.display = "none";
     }
-})
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].className = 'dot';
+    }
+    slideIdx++;
+
+    if (slideIdx > projects.length) { slideIdx = 1 };
+
+    projects[slideIdx - 1].style.display = "flex";
+    dots[slideIdx-1].className += ' active';
+    setTimeout(showSlides, 5000);
+}
+
+showSlides();
+
+// Manual slideshow
+function nextSlide(n) {
+    var nextIdx = slideIdx + n;
+    if (n < 0) {
+        thisSlide(projects.length + nextIdx);
+    } else {
+        thisSlide(nextIdx);
+    }
+}
+
+function thisSlide(num) {
+    for (var i = 0; i < projects.length; i++) {
+        projects[i].style.display = "none";
+    }
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].className = 'dot';
+    }
+    
+    slideIdx = num % 2;
+    projects[slideIdx].style.display = "flex";
+    dots[slideIdx].className += ' active';
+}
+
+// Slideshow dot clicking
+$("#one").on('click', function() {
+    thisSlide(0);
+});
+
+$("#two").on('click', function() {
+    thisSlide(1);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
